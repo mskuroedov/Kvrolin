ba31dc739e39e8a84ce326926d9eef15bda25089
mskuroedov <mskuroedov@gmail.com>
2022-07-20 16:25:44 +0300

    fix portfolio

--- src/js/porfolio.js
+++ src/js/porfolio.js
@@ -6,3 +6,32 @@ $('.portfolio-item__images .owl-carousel').owlCarousel({
     margin: 1,
     navText: ['<span></span>', '<span></span>']
 })
+
+let mobileDisplayedItems = 4,
+    maxItems = $('.portfolio-item').length
+
+function mobilePortfolioAppearance() {
+    if (window.innerWidth <= 1023 && mobileDisplayedItems !== maxItems) {
+        $('.portfolio-item').hide()
+        for (let i = 1; i <= mobileDisplayedItems; i++) {
+            $('.portfolio-item').eq(i).show()
+        }
+        $('.show-more').show()
+    } else {
+        $('.portfolio-item').show()
+        $('.show-more').hide()
+    }
+}
+
+mobilePortfolioAppearance()
+$(window).on('resize', mobilePortfolioAppearance);
+
+$('.show-more').on('click', function () {
+    mobileDisplayedItems += 4;
+    for (let i = 0; i <= mobileDisplayedItems; i++) {
+        $('.portfolio-item').eq(i).show()
+        if (i === maxItems) {
+            $(this).hide()
+        }
+    }
+})
